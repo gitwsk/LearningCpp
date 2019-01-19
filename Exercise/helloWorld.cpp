19774db11b4f3c958586e67b91da5b1dbabe3e83
siddharthkumar <siddharthkumar@192.168.1.7>
2019-01-20 00:40:19 +0530

    First Commit test

---
+++ Exercise/helloWorld.cpp
@@ -0,0 +1,19 @@
+/*
+ * helloWorld.cpp
+ *
+ *  Created on: 17-Jan-2019
+ *      Author: siddharthkumar
+ */
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int main () {
+	int a = 0;
+	auto b = a;
+	cout<< "Hello World"<<b;
+	return 0;
+}
